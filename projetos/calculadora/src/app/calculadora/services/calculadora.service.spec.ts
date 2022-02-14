fe2721bf2e69860ce5e26e05d3ffaa2065235560
Ricardo Rodrigues <rr_souza@outlook.com>
2022-02-14 13:53:36 -0300

    calculadora update

--- projetos/calculadora/src/app/calculadora/services/calculadora.service.spec.ts
+++ projetos/calculadora/src/app/calculadora/services/calculadora.service.spec.ts
@@ -1,4 +1,4 @@
-import { TestBed } from '@angular/core/testing';
+import { TestBed, inject } from '@angular/core/testing';
 
 import { CalculadoraService } from './calculadora.service';
 
@@ -6,11 +6,35 @@ describe('CalculadoraService', () => {
   let service: CalculadoraService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      providers: [CalculadoraService]
+    });
     service = TestBed.inject(CalculadoraService);
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
+  it('deve garantir que 1 + 4 = 5', inject([CalculadoraService], (service: CalculadoraService) => {
+      let soma = service.calcular(1, 4, CalculadoraService.SOMA);
+      expect (soma).toEqual(5);
+    })
+  );
+
+  it('deve garantir que 1 - 4 = -1', inject([CalculadoraService], (service: CalculadoraService) => {
+      let subtracao = service.calcular(1, 4, CalculadoraService.SUBTRACAO);
+      expect (subtracao).toEqual(-1);
+    })
+  );
+  it('deve garantir que 1 * 4 = 4', inject([CalculadoraService], (service: CalculadoraService) => {
+      let multiplicacao = service.calcular(1, 4, CalculadoraService.MULTIPLICACAO);
+      expect (multiplicacao).toEqual(4);
+    })
+  );
+  it('deve garantir que 1 / 4 = 0.25', inject([CalculadoraService], (service: CalculadoraService) => {
+      let divisao = service.calcular(1, 4, CalculadoraService.DIVISAO);
+      expect (divisao).toEqual(0.25);
+    })
+  );
 });
--- projetos/calculadora/src/app/calculadora/services/calculadora.service.ts
+++ projetos/calculadora/src/app/calculadora/services/calculadora.service.ts
@@ -1,9 +1,51 @@
+/**
+ * Serviço responsável por executar as operações da
+ * calculadora.
+ *
+ * @author Ricardo R. de Souza
+ * @since 1.0.0
+ */
+
 import { Injectable } from '@angular/core';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CalculadoraService {
+  static readonly SOMA: string = '+';
+  static readonly SUBTRACAO: string = '-';
+  static readonly DIVISAO: string = '/';
+  static readonly MULTIPLICACAO: string = '*';
 
   constructor() { }
+
+  /**
+   *
+   * @param num1 number
+   * @param num2 number
+   * @param operacao string Operação a ser executada
+   * @returns number Resultado da operação
+   */
+  calcular(num1: number, num2: number, operacao: string){
+    let resultado: number;
+
+    switch(operacao){
+      case CalculadoraService.SOMA:
+        resultado = num1 + num2;
+        break;
+      case CalculadoraService.SUBTRACAO:
+        resultado = num1 - num2;
+        break;
+      case CalculadoraService.DIVISAO:
+        resultado = num1 / num2;
+        break;
+      case CalculadoraService.MULTIPLICACAO:
+        resultado = num1 * num2;
+        break;
+      default:
+        resultado = 0;
+    }
+
+    return resultado;
+  }
 }
